4dc72629df5df28b8dc43fbf60cbc3b941b93c93
hro-ict <yasarnl81@gmail.com>
2024-03-23 13:38:18 +0100

    QR added

--- bweb1/bweb1/mfa.py
+++ bweb1/bweb1/mfa.py
@@ -1,12 +1,14 @@
 import pyotp
 import qrcode
 
+import base64
+
 
 def create_code():
     #key= pyotp.random_base32()
     key= "ThisMyScretKeyItHelpDesk"
     print(key)
-    uri= pyotp.totp.TOTP(key).provisioning_uri(name="merhaba", issuer_name="ithelpdesk")
+    uri= pyotp.totp.TOTP(key).provisioning_uri(name="django", issuer_name="ithelpdesk")
 
     print(uri)
 
@@ -19,11 +21,17 @@ def otp(token):
     print(token)
     print(result)
     
+def convert_to_base64():
+    with open("otp.png", "rb") as img_file:
+        base64_encoded_image = base64.b64encode(img_file.read()).decode()
 
-otp("908308")
+# README dosyasına QR kodunu ekleme
+    with open("README.md", "a") as readme_file:
+        readme_file.write(f"\n![OTP QR Code](data:image/png;base64,{base64_encoded_image})\n")
+
     
 
 
-
+convert_to_base64()
 
                         
